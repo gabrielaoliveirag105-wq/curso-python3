57bd51a676ead378fd9ae1212276950ffcfaac95
Gabriela Oliveira <gabrielaoliveirag105@gmail.com>
2026-08-28 21:19:09 -0300

    aula 10

--- Mundo 1/Anotações /Desafios/Aula 10/d033.py
+++ Mundo 1/Anotações /Desafios/Aula 10/d033.py
@@ -5,6 +5,19 @@ n1 = int(input('Escolha o primeiro número: '))
 n2 = int(input('Escolha o segundo número: '))
 n3 = int(input('E por fim, escolha o terceiro número: '))
 
+if (n1 >= n2 and n1 >= n3):
+    print(f'O maior número é {n1}')
+else:
+    if (n2 >= n1 and n2 >= n3):
+        print(f'O maior número é {n2}')
+    else:
+        print(f'O maior número é {n3}')
 
-
+if (n1 <= n2 and n1 <= n3):
+    print(f'O menor número é {n1}')
+else:
+    if (n2 <= n1 and n2 <= n3):
+        print(f'O menor número é {n2}')
+    else:
+        print(f'O menor número é {n3}')
     
--- Mundo 1/Anotações /Desafios/Aula 10/d034.py
+++ Mundo 1/Anotações /Desafios/Aula 10/d034.py
@@ -1,1 +1,5 @@
 print('------- DESAFIO 34 ---------')
+# Escreva um programa que prgunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00 calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+fun = str(input('Nome do funcionário:'))
+sal = float(input('Informe o salário atual: R$ '))
